6260d6a958cd9282d26b5037be1d98d1a6d5e393
FG9732 <fernando.gonz9732@gmail.com>
2022-03-13 03:49:51 -0700

    Header
    Header

--- src/frontend/reactjs/src/App.js
+++ src/frontend/reactjs/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 //import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <Header/>
+    //,
+    //<Footer/>
   );
 }
 
---
+++ src/frontend/reactjs/src/Components/Footer.js
@@ -0,0 +1,23 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+    <footer>
+        <nav className="navFooter">
+            <h3>LikeHome</h3>
+            <p> AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Log In</a></li>
+                <li><a href="#">Sign Up</a></li>
+            </ul>
+            <div>
+                Copywrite 2022 CodeMonkeys
+            </div>
+        </nav>
+    </footer>
+    );
+}
+
+export default Footer;
