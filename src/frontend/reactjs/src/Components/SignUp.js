175c0d9ba7157dd49281151750fc16d7da9d4f8f
Eric Nguyen <eric.nguyen02@sjsu.edu>
2022-03-20 23:01:54 -0700

    SignUp
    Finished SignUp portion

--- src/frontend/reactjs/src/Components/SignUp.js
+++ src/frontend/reactjs/src/Components/SignUp.js
@@ -9,23 +9,40 @@ function SignUp() {
         <div className='SignUp'>
             <h1>Sign Up</h1>
             <form>
-                <div className='SignUp-Content;'>
+                <div className='SignUp-Content'>
+                    <label class ="LabelHeader"> First Name </label>
+                    <input class = "Signup-input" type= "text" placeholder='Enter your First Name'/>
+                </div>
+
+                <div className='SignUp-Content'>
+                    <label className = "LabelHeader"> Last Name </label>
+                    <input class = "Signup-input" type= "text" placeholder='Enter your Last Name'/> 
+                </div>
+
+
+                <div className='SignUp-Content'>
+                    <label className = "LabelHeader"> Email </label>
                     <input class = "Signup-input" type= "text" placeholder='Enter your email'/>
+                </div>
+
+
+                <div className='SignUp-Content'>
+                    <label className = "LabelHeader"> Password </label>
                     <input class = "Signup-input" type= "password" placeholder='Enter a password'/>
                 </div>
+                   
+                      
+                <div> 
+                    <input className='Button' type="submit" value="Create Account"/>
+                </div>
+
+                
+
+                
             </form>
 
-         </div>
-         
-
-
-
+        </div>
     );
-
-
-
-
-
 }
 
 export default SignUp;
