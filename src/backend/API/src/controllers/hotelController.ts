3d34724b02131fdc6622a02a6b56ac63533d081f
christiandaga <christian.daga@sjsu.edu>
2022-03-25 00:53:56 -0700

    hotel endpoints

--- src/backend/API/src/controllers/hotelController.ts
+++ src/backend/API/src/controllers/hotelController.ts
@@ -1,11 +1,32 @@
 import httpStatus from 'http-status';
 
+import * as hotelServices from '../services/hotelServices';
+
 export const searchHotels = async (req: any, res: any, next: any) => {
-  const { location, checkIn, checkOut, guests } = req.query;
+  console.log('Called searchHotels method');
+
+  const { location } = req.query;
 
   try {
     // call api and get data
-    res.status(httpStatus.OK).json({'body': 'it workd!', ...req.query});
+    const searchResp = await hotelServices.search(location);
+    const destinationId = searchResp.data.suggestions[0].entities[0].destinationId;
+    delete req.query.location;
+    const resp = await hotelServices.list({...req.query, destinationId});
+    res.status(httpStatus.OK).json(resp.data.data.body.searchResults.results);
+  } catch (err) {
+    console.log('search error');
+    next(err);
+  }
+}
+
+export const listHotels = async (req: any, res: any, next: any) => {
+  console.log('Called listHotels method');
+
+  try {
+    // call api and get data
+    const resp = await hotelServices.list(req.query)
+    res.status(httpStatus.OK).json(resp.data.body.searchResults.results);
   } catch (err) {
     console.log('search error');
     next(err);
--- src/backend/API/src/routes/hotelRoutes.ts
+++ src/backend/API/src/routes/hotelRoutes.ts
@@ -5,5 +5,6 @@ import * as hotel from '../controllers/hotelController';
 const router = express.Router();
 
 router.route('/search').get(hotel.searchHotels);
+router.route('/list').get(hotel.listHotels);
 
 export default router;
