8dd211f4384ec29e669f6b09518da24c81c91ce9
asdfsdf12345 <72778737+asdfsdf12345@users.noreply.github.com>
2022-03-27 19:04:37 -0700

    working

--- src/frontend/reactjs/src/App.js
+++ src/frontend/reactjs/src/App.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import Home from './Components/Home';
-import SearchBar from './Components/SearchBar';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import SignUp from "./Components/SignUp";
-import Login from "./Components/Login";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-    <Home/>
-    </Switch>
-    </div>
-    </Router>
-    //,
-    //<Footer/>
-    //<HomePage />
-    //<SignUp />
-    //<Login />
-  );
-=======
 import './App.css';
 import React, { Component } from 'react';
 import SignUp from "./Components/SignUp";
@@ -45,7 +16,6 @@ function App() {
 
   
 
->>>>>>> Basic Signup-starting
 }
 
 export default App;
--- src/frontend/reactjs/src/Components/SignUp.js
+++ src/frontend/reactjs/src/Components/SignUp.js
@@ -1,57 +1,10 @@
 import './LoginSignUp.css';
 import React, { Component } from "react";
-<<<<<<< HEAD
-import Header from './Header';
-=======
->>>>>>> Basic Signup-starting
 //import { render } from 'react-dom';
 
 function SignUp() {
 
     return(
-<<<<<<< HEAD
-        <signup>
-            <Header>
-            </Header>
-        <div className='SignUp'>
-            <h1>Sign Up</h1>
-            <form>
-                <div className='SignUp-Content'>
-                    <label class ="LabelHeader"> First Name </label>
-                    <input class = "Signup-input" type= "text" placeholder='Enter your First Name'/>
-                </div>
-
-                <div className='SignUp-Content'>
-                    <label className = "LabelHeader"> Last Name </label>
-                    <input class = "Signup-input" type= "text" placeholder='Enter your Last Name'/> 
-                </div>
-
-
-                <div className='SignUp-Content'>
-                    <label className = "LabelHeader"> Email </label>
-                    <input class = "Signup-input" type= "text" placeholder='Enter your email'/>
-                </div>
-
-
-                <div className='SignUp-Content'>
-                    <label className = "LabelHeader"> Password </label>
-                    <input class = "Signup-input" type= "password" placeholder='Enter a password'/>
-                </div>
-                   
-                      
-                <div> 
-                    <input className='Button' type="submit" value="Create Account"/>
-                </div>
-
-                
-
-                
-            </form>
-
-        </div>
-        </signup>
-    );
-=======
         <form className='=SignUpForm'>
         <div className='SignUp'>
             <h1 className='RegistrationHeader'> Registration </h1>
@@ -72,7 +25,6 @@ function SignUp() {
 
 
 
->>>>>>> Basic Signup-starting
 }
 
 export default SignUp;
