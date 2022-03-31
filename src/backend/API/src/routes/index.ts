3b6404406b66d76e411c2824ac2c048b4abace95
Alan Nguyen <nguyenalan2000@gmail.com>
2022-03-30 17:56:20 -0700

    getReservation done?

--- src/backend/API/src/routes/index.ts
+++ src/backend/API/src/routes/index.ts
@@ -2,10 +2,11 @@ import express from 'express';
 
 import hotelRoutes from './hotelRoutes';
 // import accountRoutes from './accountRoutes';
+import reservationRoutes from './reservationRoutes'
 
 const router = express.Router();
 
 router.use('/hotel', hotelRoutes);
 // router.use('/account', accountRoutes);
-
+router.use('/reservation', reservationRoutes);
 export default router;
