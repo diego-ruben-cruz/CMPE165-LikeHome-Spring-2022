62243b2bc1f6f2d6c5e5c53b85a5109894b18dcf
Christian Daga <christian@korrelated.com>
2022-04-02 19:49:54 -0700

    payment boiler

--- src/backend/API/src/routes/index.ts
+++ src/backend/API/src/routes/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 import hotelRoutes from './hotelRoutes';
 // import accountRoutes from './accountRoutes';
+import paymentRoutes from './paymentRoutes';
 
 const router = express.Router();
 
 router.use('/hotel', hotelRoutes);
 // router.use('/account', accountRoutes);
+router.use('/payment', paymentRoutes);
 
 export default router;
