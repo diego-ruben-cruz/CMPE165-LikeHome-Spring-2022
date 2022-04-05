ba7f41e3f9997be1a8d646ddecbcc6ebcf97b2c2
Diego Cruz <diego.rubencruz@gmail.com>
2022-04-05 15:50:43 -0700

    Update Header
    Added comments to WIP code to have the LikeHome title link to the home page.

--- src/frontend/reactjs/src/Components/Header.js
+++ src/frontend/reactjs/src/Components/Header.js
@@ -7,6 +7,8 @@ function Header() {
         <>
         <nav className="navHeader">
             <label className = "label">LikeHome</label>
+            {/* <label className = "label"><Link to="/">LikeHome</Link></label> */}
+            {/* Above line is being worked on to have LikeHome title link to home page */}
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><a href="#">About Us</a></li>
