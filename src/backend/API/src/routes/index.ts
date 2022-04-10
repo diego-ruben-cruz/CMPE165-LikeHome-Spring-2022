109a8c45594a2324cdb7aa428ccd79cdf4e5e624
christiandaga <christian.daga@sjsu.edu>
2022-04-10 00:51:39 -0700

    wat

--- src/backend/API/src/routes/index.ts
+++ src/backend/API/src/routes/index.ts
@@ -2,10 +2,14 @@ import express from 'express';
 
 import hotelRoutes from './hotelRoutes';
 // import accountRoutes from './accountRoutes';
+import reservationRoutes from './reservationRoutes';
+import paymentRoutes from './paymentRoutes';
 
 const router = express.Router();
 
 router.use('/hotel', hotelRoutes);
 // router.use('/account', accountRoutes);
+router.use('/reservation', reservationRoutes);
+router.use('/payment', paymentRoutes);
 
 export default router;
