5cfcec011aeabf71cc1c5c978c4ab48026320ddc
asdfsdf12345 <72778737+asdfsdf12345@users.noreply.github.com>
2022-05-03 16:06:26 -0700

    Revert "-remerge"
    This reverts commit e12ad0bd51e72b91428b5462e87a64b1fc3c1947.

--- src/frontend/reactjs/src/Components/Footer.js
+++ src/frontend/reactjs/src/Components/Footer.js
@@ -4,79 +4,53 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 import './Footer.css';
-import { Button } from '@mui/material';
 
 const Footer = () => {
     return (
         <>
-        <Box
-          style={{width:"100%",  height:120, background: 'linear-gradient(#2B6FD4, #1b59b8)'}}
-          flexDirection = "column-reverse"
-          color="primary"
-        >
-          <Box 
-            display= "flex"
-            flexDirection="column"
+        <Box color = 'white'>
+        <Grid  style={{ background: 'linear-gradient(#2B6FD4, #1b59b8)'}} container spacing={2} px={{ xs: 3, sm:5}} py={{ xs: 3, sm:5}} color = 'white'>
+        <Grid item xs={12}>
+          <Box
+            display={'flex'}
+            justifyContent={'space-between'}
+            alignItems={'center'}
+            width={1}
+            flexDirection={{ xs: 'column', sm: 'row' }}
           >
-            <Button
-              style={{height: 30}}
-            ></Button>
-            <Typography
-              align={'center'}
-              variant={'subtitle1'}
-              color="white"
-              gutterBottom
-              justifyContent="flex-end"
-              style={{marginBottom:-50}}
+            <Box
+              display={'flex'}
+              component="a"
+              width={80}
             >
-              &copy; Code Monkeys 2022. 
-              <br/>
-              All Rights Reserved
-            </Typography>
-
             </Box>
-           
-
+            <Box display="flex" flexWrap={'wrap'} alignItems={'center'}>
+              <Box marginTop={1} marginRight={2}>
+                  Home
+              </Box>
+              <Box marginTop={1} marginRight={2}>
+                About Us
+              </Box>
+              <Box marginTop={1}>
+              </Box>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            align={'center'}
+            variant={'subtitle2'}
+            color="white"
+            gutterBottom
+          >
+            &copy; Code Monkeys. 2022. All rights reserved
+          </Typography>
+        </Grid>
+        </Grid>
         </Box>
-
-        
-       
         </>
     );
 }
 
 export default Footer;
 
-{/* <Grid  style={{ background: 'linear-gradient(#2B6FD4, #1b59b8)'}} container spacing={2} px={{ xs: 3, sm:5}} py={{ xs: 3, sm:5}} color = 'white'>
-<Grid item xs={12}>
-  <Box
-    display={'flex'}
-
-    justifyContent={'space-between'}
-    alignItems={'center'}
-    width={1}
-    flexDirection={{ xs: 'column-reverse', sm: 'row' }}
-  >
-    <Box
-      display={'flex'}
-      component="a"
-      width={80}
-    >
-    </Box>
-      <Box marginTop={1}>
-      </Box>
-    </Box>
-</Grid>
-<Grid item xs={12}>
-  <Typography
-    align={'center'}
-    variant={'subtitle1'}
-    color="white"
-    gutterBottom
-  >
-    &copy; Code Monkeys 2022. 
-    <br/>
-    All Rights Reserved
-  </Typography>
-</Grid>
-</Grid> */}
